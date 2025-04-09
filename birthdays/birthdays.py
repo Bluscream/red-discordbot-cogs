b17aae9d87896908009b681314e66ddb5e730083
Bluscream <Bluscream@users.noreply.github.com>
2025-04-09 20:07:47 +0200

    t

--- birthdays/birthdays.py
+++ birthdays/birthdays.py
@@ -112,8 +112,11 @@ class Birthdays(commands.Cog):
     def _get_next(self, dt: date):
         now = datetime.now().date()
         if isinstance(dt, datetime): dt = dt.date()
-        log.error(f"dt: {dt}, dt <= now: {dt <= now}, now: {now}")
-        if dt <= now: dt = date(now.year + 1, dt.month, dt.day) # Todo: wait for https://github.com/Rapptz/discord.py/pull/9685
+        # log.error(f"dt: {dt}, dt <= now: {dt <= now}, now: {now}")
+        if dt <= now: # If the date is in the past, add a year to make it next year's date
+            dt = date(now.year + 1, dt.month, dt.day)
+        elif dt > date(now.year + 1, dt.month, dt.day): # If the date is in the future but more than a year away, subtract a year
+            dt = date(now.year, dt.month, dt.day)
         return dt
 
     async def _create_event(self, ctx, dt: date):
