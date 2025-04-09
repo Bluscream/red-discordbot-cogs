63645f225da167dd0f80be89c24e18bf319ddfea
Bluscream <Bluscream@users.noreply.github.com>
2025-04-09 19:53:37 +0200

    a

--- birthdays/birthdays.py
+++ birthdays/birthdays.py
@@ -110,8 +110,8 @@ class Birthdays(commands.Cog):
         raise ValueError(lang.get("error.invalid_date_format").format(date_str=date_str))
 
     def _get_next(self, dt: date):
-        now = datetime.now()
-        if dt <= date.today(): dt = date(now.year + 1, dt.month, dt.day) # Todo: wait for https://github.com/Rapptz/discord.py/pull/9685
+        now = date.now().date()
+        if dt <= now: dt = date(now.year + 1, dt.month, dt.day) # Todo: wait for https://github.com/Rapptz/discord.py/pull/9685
         return dt
 
     async def _create_event(self, ctx, dt: date):
